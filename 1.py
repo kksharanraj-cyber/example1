295ede4b8e7ba40c7f530ac33a38bf6942306064
ajaya <kksharanraj@gmail.com>
2026-03-13 13:27:08 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,11 +1,2 @@
 print("welcome to college")
 print("ajay rapper")
-name: Run Python Script
-on: [push]
-jobs:
-  run:
-    runs-on: ubuntu-latest
-    steps:
-      - uses: actions/checkout@v2
-      - uses: actions/setup-python@v2
-      - run: python 1.py
